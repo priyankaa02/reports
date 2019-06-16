6da92c7632e55a05b63f40ca7b7a8fb8f98367b3
priyanka <priyankaa0207@gmail.com>
2019-06-16 22:49:18 +0530

    more change

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var user_controller = require('../controllers/user_controller');
+var unit_controller = require('../controllers/unitController');
 
 router.get('/largest/invoice', [user_controller.largestInvoice]);
-router.get('/largest/units', [user_controller.largestUnits]);
+router.get('/largest/units', [unit_controller.largestUnits]);
 
 module.exports = router;
